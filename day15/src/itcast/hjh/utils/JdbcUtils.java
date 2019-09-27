cf8088ddf860508f8e28264177edab8cb24f5717
KongBowGeeBin <hjh163-nba@163.com>
2019-09-27 18:06:12 +0800

    first push

---
+++ day15/src/itcast/hjh/utils/JdbcUtils.java
@@ -0,0 +1,47 @@
+package itcast.hjh.utils;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class JdbcUtils {
+
+    private static Properties config = new Properties();
+    static {
+        try {
+            config.load(JdbcUtils.class.getClassLoader().getResourceAsStream("db.properties"));
+        }catch (Exception e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(config.getProperty("url"),config.getProperty("username"),config.getProperty("password"));
+    }
+
+    public static void release(Connection conn, Statement st, ResultSet rs) {
+        if (rs!=null) {
+            try{
+                rs.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            rs = null;
+        }
+        if (st!=null) {
+            try{
+                st.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            st = null;
+        }
+        if (conn!=null) {
+            try{
+                conn.close();
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+            conn = null;
+        }
+    }
+}
