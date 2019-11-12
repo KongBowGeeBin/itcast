69d6244d704d7d8d181d01d89d13c629b302be7e
KongBowGeeBin <hjh163-nba@163.com>
2019-11-12 18:05:37 +0800

    day09 1112

---
+++ day09_user/src/itcast/hjh/dao/impl/UserDaoImpl.java
@@ -0,0 +1,30 @@
+package itcast.hjh.dao.impl;
+
+import itcast.hjh.domain.User;
+
+public class UserDaoImpl {
+
+    public void add(User user){
+
+    }
+
+    public User find(String username,String password){
+
+        return null;
+    }
+
+    public User find(String username){
+
+        return null;
+    }
+
+    public void delete(){
+
+    }
+
+    public User update(){
+
+        return null;
+    }
+
+}
